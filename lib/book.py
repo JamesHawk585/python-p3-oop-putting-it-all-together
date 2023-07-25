732de269ec97e6df77f307f792e47454bb43729c
James Hawk <jph94880@gmail.com>
2023-07-25 18:19:31 -0500

    Modifies Book class

--- lib/book.py
+++ lib/book.py
@@ -4,8 +4,10 @@ class Book:
     def __init__(self, title, page_count):
         self.title = title 
         self.page_count = page_count 
+        page_count = int(page_count)
 
 
 harry_potter = Book("Harry Potter", 100)
 
-harry_potter
+harry_potter.page_count
+
